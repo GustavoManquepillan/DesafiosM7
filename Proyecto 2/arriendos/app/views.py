724c1d8fdfbb796f13ac366cbc80695dee564bfa
Gustavo Manquepillán <gustavomanquepillan@gmail.com>
2024-06-07 20:58:41 -0400

    subiendo hito 2 otra vez

--- Proyecto 2/arriendos/app/views.py
+++ Proyecto 2/arriendos/app/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from .models import Inmueble
-
+from .forms import UserForm
 def index(request):
     inmuebles = Inmueble.objects.all()
     context = {
         'inmuebles': inmuebles
     }
     return render(request, 'index.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')   
+    else:
+        form = UserForm()
+    context =   {'form': form}  
+    return render(request,'registration/register.html', context)
