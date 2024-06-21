51de79c72214562ecb3db61421a81cbcf86e55eb
GustavoManquepillan <157856261+GustavoManquepillan@users.noreply.github.com>
2024-06-21 19:26:26 -0400

    Delete Dia 1 directory

--- Dia 1/Desafío Guiado/desafiodb/testdb/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class AdlTest(models.Model):
-    campo1 = models.CharField(max_length=100)
-    valor1 = models.IntegerField()
--- Dia 1/Ejercicios/proyecto_capitulo_1/testadl/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestadlConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'testadl'
